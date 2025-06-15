72a1372797194a5d9e67fd7a6cd59ccb4aab7859
DemigodKS <mankok222@gmail.com>
2025-06-15 16:37:46 +0300

    Update botik.py

--- botik.py
+++ botik.py
@@ -3,16 +3,13 @@ import asyncio
 from handlers import router_handlers
 from states import router_state
 
-#создание экземпляра бота
-bot = Bot(token=" ")
-dp = Dispatcher()
 #запуск
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     # создание экземпляра бота
-    bot = Bot(token="8140173668:AAG9kiA0f22gv_7p-Z4-Hk-y2GYykQ8EsXE")
+    bot = Bot(token=" ")
     dp = Dispatcher()
 
     dp.include_router(router_handlers)
